95f32511627ffcdab39c71964e0df83a855ec9c7
RupeshPradhan369 <140105702+RupeshPradhan369@users.noreply.github.com>
2026-04-10 02:17:31 +0545

    fix secret key fallback

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='fallback-secret-key-for-dev-only')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
